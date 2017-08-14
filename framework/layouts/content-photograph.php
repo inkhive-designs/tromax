b6765470a8574ba60968b3df8faedd95937b4089
Shiv Kumar <shiv@inkhive.com>
2017-08-14 16:58:52 +0530

    Default Blog Layout Added To New Blog Layout Tromax

--- framework/layouts/content-photograph.php
+++ framework/layouts/content-photograph.php
@@ -7,7 +7,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-6 col-sm-6 grid grid_2_column photograph'); ?>>
 
     <div class="featured-thumb col-md-12 col-sm-12">
-        <?php the_post_thumbnail('tromax-pop-thumb'); ?>
+        <?php if (has_post_thumbnail()) : ?>
+            <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('tromax-pop-thumb'); ?></a>
+        <?php else: ?>
+            <a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><img src="<?php echo get_template_directory_uri()."/assets/images/placeholder2.jpg"; ?>"></a>
+        <?php endif; ?>
     </div><!--.featured-thumb-->
 
     <div class="out-thumb col-md-12 col-sm-12">
